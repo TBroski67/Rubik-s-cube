b2f979bffbe54309a9ce84cea68f9f2acaccae1e
TBroski67 <tkuang2@nastudents.org>
2026-07-14 20:20:29 +0000

    modified:   test.py

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
+"""INSTRUCTIONS FOR USAGE:
+When you run this file, it should first display an unfolded cube, with
+the sides laid out in a sideways lowercase t shape. To verify the next
+test case, hold a solved cube with the yellow side on top and the blue
+side in front. Rotate the yellow side clockwise, the blue side 180
+degrees, and the yellow side clockwise again. It should match with the
+output from the second print_cube command. The third test case will
+generate a random scramble. Pieces with the same color should not be
+next to each other on adjacent faces.
+
+You may comment out any sections of code to focus on specific test cases.
+"""
 from display import print_cube
 import rotations
 from time import sleep
