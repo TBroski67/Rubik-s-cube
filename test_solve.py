e53f1c7deb58360ecbc003199cbfad78df642c98
TBroski67 <tkuang2@nastudents.org>
2026-08-05 16:01:17 -0400

    Update test_solve.py

--- test_solve.py
+++ test_solve.py
@@ -1,7 +1,12 @@
+'''This file uses a constant scramble (always the same scramble)
+and uses the solving algorithm to attempt to solve the scramble.
+Once the solver is complete, this program should produce a solved
+cube as the final result.'''
 import rotations
 from display import print_cube
 from cross import solve_cross
-#scramble from online cube timer
+from f2l import solve_f2l
+#scramble generated on an online cube timer website
 rotations.uTwo()
 rotations.r()
 rotations.u()
@@ -22,8 +27,8 @@ rotations.lTwo()
 rotations.rw()
 rotations.u()
 rotations.fTwo()
-#display cube
 print_cube()
-#apply solving alogrithms, then display cube
 solve_cross()
+print_cube()
+solve_f2l()
 print_cube()
